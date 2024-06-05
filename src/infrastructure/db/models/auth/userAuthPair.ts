c30f040ad703ea1ed47d5032e0806d75067bbf9c
haruki-nikaidou <meteo.haru@gmail.com>
2024-06-05 07:06:05 -0400

    add primary key for auth pair

--- src/infrastructure/db/models/auth/userAuthPair.ts
+++ src/infrastructure/db/models/auth/userAuthPair.ts
@@ -1,7 +1,10 @@
-import {pgTable, uuid, varchar} from 'drizzle-orm/pg-core';
+import {index, pgTable, primaryKey, uuid, varchar} from 'drizzle-orm/pg-core';
 
 export const userAuthPair = pgTable('user_auth_pair', {
   authProvider: varchar('auth_provider', {length: 32}).notNull(),
   authKey: varchar('auth_key', {length: 256}).notNull(),
   userId: uuid('user_id').notNull(),
-})
+}, table => ({
+  reverseUserIndex: index('reverse_user_index').on(table.userId),
+  primary: primaryKey({columns: [table.authKey, table.authProvider]})
+}))
